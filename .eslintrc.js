979cab2edd86e27b573be6d909754339a5c8d2f4
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-07-02 09:33:11 -0300

    :sparkles: feat - Navbar

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,11 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
 };
